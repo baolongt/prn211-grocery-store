9f37b92d23faf40fd80e4f17a14cfa0925b053cc
DatQuoc <datquoc3003@gmail.com>
2022-03-19 20:29:34 +0700

    add register

---
+++ PRN211_Grocery_store/Data/Entity/UserRegister.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRN211_Grocery_store.Data.Entity
+{
+    public class UserRegister
+    {
+        [Key]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Phone { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ PRN211_Grocery_store/Models/DAO/UserRegisterDAO.cs
@@ -0,0 +1,43 @@
+﻿using PRN211_Grocery_store.Data;
+using PRN211_Grocery_store.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRN211_Grocery_store.Models.DAO
+{
+    public class UserRegisterDAO
+    {
+        private static UserRegisterDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static UserRegisterDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null) instance = new UserRegisterDAO();
+                    return instance;
+                }
+            }
+
+        }
+        public User CheckUserDuplicate(string email)
+        {
+            User user = null;
+            try
+            {
+                using (var context = new ApplicationDBContext())
+                {
+                    user = context.Users.SingleOrDefault((user) => user.Email == email);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return user;
+        }
+    }
+}
